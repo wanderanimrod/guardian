ec1314530ea263e479ce1f7211734848867b75f6
Nimrod Wandera <wanderanimrod@gmail.com>
2019-05-20 00:18:58 -0500

    successfully disallows pods. Deployments seem to get created anyways

--- guardian/lib/guardian_web/controllers/validation_controller.ex
+++ guardian/lib/guardian_web/controllers/validation_controller.ex
@@ -2,15 +2,26 @@ defmodule GuardianWeb.ValidationController do
   use GuardianWeb, :controller
 
   require Logger
-  def post(conn, _) do
+  def post(conn, params) do
     conn
     |> put_status(200)
-    |> json(%{status: "ok"})
+    |> json(disallow_all(params))
   end
 
   def health(conn, _) do
     conn 
     |> put_status(200) 
     |> json(%{status: "The guardian is on guard 😸"})
+  end
+
+  defp disallow_all(admission_review_request) do
+    admission_denied = %{
+      allowed: false,
+      status: %{
+        status: "Failure",
+        message: "This cluster is closed for business. No deployments allowed."
+      }
+    }
+    Map.put(admission_review_request, :response, admission_denied)
   end
 end
